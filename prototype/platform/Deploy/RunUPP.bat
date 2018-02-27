b30c59f40bc965dba97c23b60ea9ebcd0a299fd9
rpaddock <rpaddock@prowestgis.com>
2018-02-27 15:26:57 -0600

    Shortcut bat file to launch the Micro-Services.

---
+++ prototype/platform/Deploy/RunUPP.bat
@@ -0,0 +1,8 @@
+start "Manager" /D "..\Manager\bin\Debug\" Manager.exe
+start "Service Directory" /D "..\ServiceDirectory\bin\Debug\" ServiceDirectory.exe
+start "Company Info" /D "..\CompanyInformation\bin\Debug\" CompanyInformation.exe
+start "Vehicle Info" /D "..\VehicleInformation\bin\Debug\VehicleInformation.exe
+start "Trailer Info" /D "..\TrailerInformation\bin\Debug\" TrailerInformation.exe
+start "Truck Info" /D "..\TruckInformation\bin\Debug\" TruckInformation.exe
+start "Insurance Info" /D "..\InsuranceInformation\bin\Debug\" InsuranceInformation.exe
+start "Axle Info" /D "..\AxleInformation\bin\Debug\" AxleInformation.exe
