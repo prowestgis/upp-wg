bdbaaddd30b1939d66c6f83b6c97d2a8cf58437e
rpaddock <rpaddock@prowestgis.com>
2018-02-28 15:42:20 -0600

    Changed the data type of the user_id field.  Work around for the dapper query error when converting from blob to string.

--- prototype/platform/Manager/App_Data/Schema.sql
+++ prototype/platform/Manager/App_Data/Schema.sql
@@ -3,14 +3,14 @@
 -- to map UPP authorized users to existing resources.
 create table Users
 (
-	user_id                            blob not null           -- GUID under control of UPP
+	user_id                            varchar(200) not null           -- GUID under control of UPP
 );
 
 -- Table of external id tokens used to track multiple identities associated with 
 -- each UPP user
 create table ExternalLogins
 (
-    user_id                            blob not null,          -- FK to the Users table
+    user_id                            varchar(200) not null,  -- FK to the Users table
 	provider_id                        varchar(100) not null,  -- OAuth2 provider FK
 	provider_user_id                   varchar(100) not null   -- Identity of the user on the external system
 );
