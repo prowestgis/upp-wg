8387c1135de8aab9ac5f0e74f7823705dc0dd941
Lucas Scharenbroich <lscharen@prowestgis.com>
2018-03-23 16:06:27 -0500

    Streamline the configuration of a permit issuer in order to fully configure difference via the command line

--- prototype/platform/PermitIssuer/Bootstrapper.cs
+++ prototype/platform/PermitIssuer/Bootstrapper.cs
@@ -6,6 +6,7 @@ using Newtonsoft.Json;
 using UPP.Configuration;
 using UPP.Common;
 using System.Configuration;
+using System;
 
 namespace PermitIssuer
 {
@@ -21,24 +22,21 @@ namespace PermitIssuer
         protected override void ConfigureApplicationContainer(TinyIoCContainer container)
         {
             base.ConfigureApplicationContainer(container);
-
-            // Bootstrap our application services
-            //logger.Debug("Registering database singleton");
-            //container.Register(new Database());
         }
 
         protected override void ApplicationStartup(TinyIoCContainer container, Nancy.Bootstrapper.IPipelines pipelines)
         {
             base.ApplicationStartup(container, pipelines);
 
-            // Initialize the database
-            //logger.Debug("ApplicationStartup: Initializing the database");
-            //container.Resolve<Database>().Initialize();
-
             // Register a callback so we can periodically try to register ourselves with the Service Directory.  This is
             // am improper hijack of the request pipeline, but *much* easier than setting up a real background monitor task
             // for prototype development.
             var config = container.Resolve<HostConfigurationSection>();
+
+            // Add a default value hook to set the default scope to "permit.approval.{Self:Identifier}" if no scope
+            // if set in the app.config or command line.
+            config.SetDefaultValue(Keys.SERVICE_DIRECTORY__SCOPES, () => String.Format("permit.approval.{0}", config.Keyword(Keys.SELF__IDENTIFIER)));
+
             pipelines.BeforeRequest.AddItemToStartOfPipeline(RegisterWithServiceDirectory.GetPipelineHook(UPP_IDENTITY, config));
         }
     }
