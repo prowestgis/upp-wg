663c6c9d294fa0516a7a7b96fa1f6775c2ffaf60
Lucas Scharenbroich <lscharen@prowestgis.com>
2018-06-15 07:42:45 -0500

    Initial implementation of OnBefore handler to allow local access regardless of authentication

--- prototype/platform/AxleInformation/AxleInformationModule.cs
+++ prototype/platform/AxleInformation/AxleInformationModule.cs
@@ -1,12 +1,35 @@
 ﻿using Nancy;
 using Nancy.Security;
+using Nancy.Extensions;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using UPP.Security;
+using UPP.Common;
 
 namespace AxleInformation
 {
+    public static class SecurityHooks
+    {
+        public static void AllowFromLocalHost(this INancyModule module)
+        {
+            module.AddBeforeHookOrExecute(SecurityHooks.CheckIsLocal(), "Allow from localhost");
+        }
+
+        private static Func<NancyContext, Response> CheckIsLocal()
+        {
+            return (ctx) =>
+            {
+                if (ctx.Request.IsLocal() && !ctx.CurrentUser.IsAuthenticated())
+                {
+                    ctx.CurrentUser = new NancyAuthUser(new AuthToken() { Sub = "local", Email = "nobody@example.com" });
+                }
+
+                return null;
+            };
+        }
+    }
     /// <summary>
     /// The agent API allows trusted hosts to manage their metadata
     /// </summary>
@@ -14,6 +37,9 @@ namespace AxleInformation
     {
         public AxleInformationModule(Database database) : base("/api/v1/axles")
         {
+            // Allow from localhost unconditionally
+            this.AllowFromLocalHost();
+
             // Need a valid JWT to access
             this.RequiresAuthentication();
 
